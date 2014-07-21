dca33f2accdac854dfb8809f8e4201008192dd52
David Keijser <david.keijser@klarna.com>
2014-07-21 14:14:51 +0200

    add some cluster stuff

--- example.js
+++ example.js
@@ -2,26 +2,48 @@ var config = require('config'),
     logging = require('rapidus'),
     express = require('express'),
     logger = logging.getLogger('app'),
+    cluster = require('cluster'),
     app = express();
 
 require('rapidus-configure')(config.logging);
 
-app.use(logging.getLogger('access').middleware);
+function worker() {
+    logger.info('worker online', process.pid);
+    app.use(logging.getLogger('access').middleware);
 
-app.get('/test', function (req, res, next) {
-    logger.info('processing request');
-    setTimeout(function () {
-        logger.info('request processed');
-        res.send(200, 'zoidberg');
-    }, 100);
-});
+    app.get('/test', function (req, res, next) {
+        logger.info('processing request');
+        setTimeout(function () {
+            logger.info('request processed');
+            res.send(200, 'zoidberg');
+        }, 100);
+    });
 
-app.listen(4000);
+    app.listen(4000);
+}
 
-// Log some stuff
-logger.debug('some details');
-logger.warn('this feels bad');
-logger.error('abandon ship');
+function master() {
+    var logger = logging.getLogger('master');
 
-logging.getLogger('app.other').info('not visible');
-logging.getLogger('app.other').warn('visible');
+    for (var i = 0; i < 4; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function (worker) {
+        logger.info('%s has left the building', worker.process.pid);
+    });
+
+    // Log some stuff
+    logger.debug('some details');
+    logger.warn('this feels bad');
+    logger.error('abandon ship');
+
+    logging.getLogger('app.other').info('not visible');
+    logging.getLogger('app.other').warn('visible');
+}
+
+if (cluster.isMaster) {
+    master();
+} else {
+    worker();
+}
